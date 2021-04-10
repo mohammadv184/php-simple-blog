ed340fb9c619f41ed7fb3cb9a1486831bc5ce230
mohammad abbasi <mohammad.v184@gmail.com>
2021-04-10 18:30:48 +0430

    add post class and methods

--- app/models/Post.php
+++ app/models/Post.php
@@ -11,16 +11,37 @@ class Post
 
     public function getPage($page_number, $per_page)
     {
-        // TODO: Implement getPage method here
+        $offset = ($page_number - 1) * $per_page;
+        $sql = "
+SELECT id, title, author_name, posted_at, image_name, content FROM posts
+ORDER BY posted_at DESC LIMIT $per_page OFFSET $offset ;";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function countAll()
     {
-        // TODO: Implement countAll method here
+        $sql = "SELECT COUNT(*) AS cnt FROM posts ;";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        return $row['cnt'];
     }
 
     public function get($id)
     {
-        // TODO: Implement get method here
+         $sql = "
+SELECT id, title, author_name, posted_at, image_name, content FROM posts
+WHERE id = :id ;";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':id', $id);        
+        $stmt->execute();
+        $row = null;
+        if ($stmt->rowCount() > 0) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        return $row;
+        
     }
 }
